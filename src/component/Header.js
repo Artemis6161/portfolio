95cc1ef9d57b6fe143d337028d1db5d0d086b3d2
sangeetha <Artemis6161@github.com>
2021-11-24 18:32:36 +0530

    t

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import Image1 from './img/009 a.jpg';
 import { Button } from '@chakra-ui/button';
 import { useColorMode } from '@chakra-ui/color-mode';
 import {Image, Img} from '@chakra-ui/image';
-import {Circle,Stack,Flex,Box,Text} from "@chakra-ui/layout"
+import {Circle,Stack,Flex,Box,Text} from "@chakra-ui/layout";
+import {useMediaQuery} from '@chakra-ui/media-query';
 const Header = () => {
     const { colorMode } = useColorMode();
     const isDark = colorMode === "dark";
+    const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)")
+
   return (
    <Stack>
+     {isNotSmallerScreen && (
+ <Circle
+ position="absolute"
+ bg="blue.100" 
+ opacity="0.1"
+ w="300px"
+ h="300px"
+ alignSelf="flex-end"/>
+     )}
         <Circle
         position="absolute"
         bg="blue.100" 
@@ -15,14 +28,28 @@ const Header = () => {
         w="300px"
         h="300px"
         alignSelf="flex-end"/>
-        <Flex>
-        <Box>
-        <Text>Hi,</Text>
-        <Text>I'm Sangeetha Rajan.</Text>
-        <Text> I design and Code simple yet beautiful websites.</Text>
-        <Button>Hire Me</Button>
+        <Flex direction={isNotSmallerScreen ? "row" : "column"}
+        spacing="200px"
+        p={isNotSmallerScreen ? "32" : 0}
+        alignSelf="flex-start">
+        <Box mt={isNotSmallerScreen?0:16} alignself="flex-start">
+          <Text fontSize="5xl" fontWeight="semibold">Hi,</Text>
+        <Text fontSize="6xl"
+        fontWeight="bold"
+        bgGradient="linear(to-r,cyan.400,blue.500,purple.600)"
+        bgClip="text">I'm Sangeetha Rajan.</Text>
+        <Text color={isDark ? "gray.200" : "gray.500"}> I design and Code simple yet beautiful websites.</Text>
+        <Button mt={8} colorScheme="blue">Hire Me</Button>
         </Box>
-        <Img src="img/009 a.jpg" alt="image"></Img>
+        <Image mt={isNotSmallerScreen?"0" : "12"}
+        mb={isNotSmallerScreen?"0" : "12"}
+        alignSelf="center"
+        borderRadius="full"
+        backgroundcolor="transparent"
+        boxShadow="lg"
+        boxSize="300"
+        zIndex={1}
+        src={Image1}alt="image"/>
         </Flex>
         </Stack>
         
